16ccf6c7b30092c03f65d8c7d3efdda0c9861f78
Shahzod9941 <shahzodtoato@gmail.com>
2020-01-07 11:10:37 +0500

    Initial version

---
+++ src/js/app.js
@@ -0,0 +1,10 @@
+const regularPurchases = 3000000
+const regularPurchasesPersent =  0.01
+const cashback = regularPurchases * SpeechRecognitionResult
+console.log(cashback)
+if (regularPurchases * regularPurchasesPersent > 3000){
+    const cashback = 3000;
+}
+else{
+    const cashback = regularPurchases * regularPurchasesPersent;
+}
